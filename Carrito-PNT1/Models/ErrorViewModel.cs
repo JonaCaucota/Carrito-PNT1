7723247f0944b31ff9e3a83cfbf0d4ae85883d55
JonaCaucota <jonacau12@gmail.com>
2023-01-28 20:22:48 -0300

    added ErrorViewModel messages

--- Carrito-PNT1/Models/ErrorViewModel.cs
+++ Carrito-PNT1/Models/ErrorViewModel.cs
@@ -5,5 +5,11 @@ namespace Carrito_PNT1.Models
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        //Erorres Personalizados
+        public const string CampoRequerido = "{0} es obligatorio";
+        public const string CaracteresMinimos = "{0} no debe exceder las {1} caracteres.";
+        public const string CaracteresMaximos = "{0} no debe superar los {1} caracteres.";
+        public const string PrecioValido = "{0} debe ser mayor a ${1} y menor a {2}";
     }
 }
